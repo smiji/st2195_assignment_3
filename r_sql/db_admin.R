9556c61be769954796abfcb31d7b7f366a7e03ac
Smiji John <smiji.j@gmail.com>
2024-01-02 00:58:31 +0000

    Create db and output files
    The airline.db script and output csvs

--- r_sql/db_admin.R
+++ r_sql/db_admin.R
@@ -9,14 +9,14 @@ remove_db_if_exists <- function (path){
   }  
 }
 
-create_tables_from_csv <- function(path_to_db){
-  library(DBI)
-  conn = dbConnect(RSQLite::SQLite(),path_to_db)
+merge_data_frame <- function (){
   path_csv = "/Users/smiji.j/sqlite_dbs/db"
-  prepare_tables_from_csv(conn,path_csv,"student.csv","Student")
-  prepare_tables_from_csv(conn,path_csv,"course.csv","Course")
-  prepare_tables_from_csv(conn,path_csv,"grade.csv","Grade")
-  dbDisconnect(conn)
+  setwd(path_csv)
+  df_for_csv1 <- read.csv("2006.csv",header = TRUE)
+  df_for_csv2 <- read.csv("2007.csv",header = TRUE)
+  df_for_csv3 <- read.csv("2008.csv",header = TRUE)
+  merged_df <- rbind(rbind(df_for_csv1,df_for_csv2),df_for_csv3)
+  return (merged_df) 
 }
 
 prepare_tables_from_csv<-function(conn,working_dir,csv_name,table_name){
@@ -25,18 +25,30 @@ prepare_tables_from_csv<-function(conn,working_dir,csv_name,table_name){
   dbWriteTable(conn,table_name,df_for_csv)
 }
 
-summary_of_tables_created <- function(path_to_db){
+
+create_tables_from_csv <- function(path_to_db){
   library(DBI)
   conn = dbConnect(RSQLite::SQLite(),path_to_db)
-  print(dbReadTable(conn,"Grade"))
+  path_csv = "/Users/smiji.j/sqlite_dbs/db"
+  prepare_tables_from_csv(conn,path_csv,"airports.csv","airports")
+  prepare_tables_from_csv(conn,path_csv,"carriers.csv","carriers")
+  prepare_tables_from_csv(conn,path_csv,"plane-data.csv","planes")
+  dbWriteTable(conn,'ontime',merge_data_frame())
   dbDisconnect(conn)
 }
 
-path = "/Users/smiji.j/sqlite_dbs/University.db"
+
+summary_of_tables_created <- function(path_to_db){
+  library(DBI)
+  conn = dbConnect(RSQLite::SQLite(),path_to_db)
+  print(dbReadTable(conn,"planes"))
+  dbDisconnect(conn)
+}
+
+path = "/Users/smiji.j/sqlite_dbs/airline.db"
 remove_db_if_exists(path)
 create_tables_from_csv(path)
 summary_of_tables_created(path)
-
 
 
 #setwd("/Users/smiji.j/DataScienceProgramming/st2195_assignment_3/r_sql")
